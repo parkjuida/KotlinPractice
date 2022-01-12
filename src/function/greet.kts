b57b60f72148d5b397f45e4816d16119606abd9a
parkjuida <bees1114@naver.com>
2022-01-12 23:31:09 +0900

    mend

--- src/function/greet.kts
+++ src/function/greet.kts
@@ -6,4 +6,14 @@ println(hello())
 
 
 var returnString = hello()
-print("return string is $returnString")
+println("return string is $returnString")
+
+fun unitFunction() {
+}
+println("result of unit function: ${unitFunction()}")
+
+fun functionHasParameter(arg: String): String {
+    return arg
+}
+println("parameter function: ${functionHasParameter("Hello")}")
+
