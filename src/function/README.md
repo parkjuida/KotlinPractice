6b0bece4e30143fab04d42f6f6418caa1892fea3
parkjuida <bees1114@naver.com>
2022-01-16 23:15:13 +0900

    함수

--- src/function/README.md
+++ src/function/README.md
@@ -27,3 +27,54 @@ fun functionHasParameter(arg: String) {
 리턴 타입은 생략할 수 있지만 파라미터의 타입은 생략할 수 없다.
 또한 파라미터의 값을 바꾸는것도 금지 (컴파일에러 발생) 되어있다.
 
+#### 기본 아규먼트
+코틀린에서는 함수에 기본 아규먼트를 할당할 수 있다.
+```kotlin
+fun functionHasArgument(arg: String = "Hello") {
+}
+```
+
+#### 명시적 아규먼트
+```kotlin
+fun functionA(argA: String, argB: String) = "$argA $argB"
+``` 
+함수 아규먼트이름을 명시해서 호출할 수도 있다. 
+이 특성은 가독성을 높여주고 함수 변경을 더 쉽게 할 수 있도록 도와준다.
+
+#### 다중 인자와 스프레드
+코틀린에서 많은 수의 인자를 받는 방법은 아래와 같다.
+```kotlin
+fun max(vararg numbers: Int): Int {
+  var maximum = Int.MIN_VALUE
+  for(number in numbers) {
+    maximum = if(number > maximum) number else maximum
+  }
+  return maximum
+}
+```
+vararg 키워드를 명시한 함수의 파라미터는 배열 형태로 값을 받는다.
+vararg 키워드는 어느 위치에 사용되어도 상관 없지만 마지막에 사용하는게 좋다.
+```kotlin
+fun multiParameter(msg: String, vararg messages) {
+}
+multiParameter("A", "B", "C", "D")
+```
+위와 같이 사용하면 "A"는 msg, ["B", "C", "D"]는 message로 할당이된다.
+하지만 vararg를 앞에 사용하면 msg는 꼭 명시적 인자를 사용해야한다.
+```kotlin
+fun multiParameter(vararg messages:String, msg: String) {
+}
+multiParameter("A", "B", "C", msg="D")
+```
+
+### 구조분해
+```kotlin
+fun getName() = Triple("Park", "Kim", "Son")
+val result = getName()
+println("$result.first $result.second $result.third")
+
+val (park, kim, son) = getName()
+```
+코틀린은 객체의 값을 추출해 변수로 바로 넣는 구조분해 기능이 있다.
+위 예제에서 볼 수 있듯이 Triple을 세 개의 변수에 할당할 수 있다.
+위치로 구분하여 할당 할 수 있고 사용하지 않을 변수는 _을 이용해서 무시할 수 있다.
--- src/function/greet.kts
+++ src/function/greet.kts
@@ -17,3 +17,57 @@ fun functionHasParameter(arg: String): String {
 }
 println("parameter function: ${functionHasParameter("Hello")}")
 
+
+// 기본 아규먼트
+fun greet(name: String, msg: String = "Hello"): String = "$msg $name"
+println("기본 아규먼트" + greet("park"))
+println("기본 아규먼트 2" + greet("park", " Hi"))
+
+fun greet2(name: String="park", msg: String): String = "$msg $name"
+println("hi")
+println(greet2(msg="abc"))
+
+// 명시적 아규먼트
+fun createItem(name: String, quantity: Int, price: Int, discountRate: Double) {
+    print("name: $name quantity: $quantity price: $price discountRate: $discountRate")
+}
+print("명시적 아규먼트")
+createItem("Apple", 100, 1000, 0.0)
+createItem(name="Apple", quantity = 100, price=1000, discountRate=0.0)
+createItem("Apple", 100, discountRate = 0.0, price=1000)
+
+// 다중인자
+fun max(vararg numbers: Int): Int {
+    var maximum = Int.MIN_VALUE
+    for(number in numbers) {
+        maximum = if(number > maximum) number else maximum
+    }
+    return maximum
+}
+println("다중인자")
+println(max(1, 2, 3, 4, 5))
+
+fun multiParameter(vararg numbers: Int, msg: String): String = "${numbers.toString()} $msg"
+multiParameter(1,2,3, msg="A")
+
+fun multiParameter2(msg: String, vararg messages: String) = "${messages.joinToString(", ")} $msg"
+multiParameter2("A", "B", "C", "D")
+
+// 스프레드 연산자
+val numberList = intArrayOf(1, 2, 3, 4, 5)
+println("스프레드 연산자")
+println(max(*numberList))
+
+// 구조분해
+fun getName() = Triple("Park", "Kim", "Son")
+val result = getName()
+println("${result.first} ${result.second} ${result.third}")
+
+val (park, kim, son) = getName()
+println("$park $kim $son")
+
+val (a12, _, c1) = getName()
+println("$a12 $c1")
+
+val (a1, b1) = getName()
+println("$a1 $b1")
